f3f9de447be23945b1c57fe9df69205ef93c6037
Ruairi O Brien <ruairitobrien@gmail.com>
2020-06-02 21:41:47 +0100

    Added err on no arg test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn execute(args: Vec<String>) -> Result<(), ()> {
+    Err(())
+}
+
+#[cfg(test)]
+mod test {
+
+    use crate::execute;
+
+    #[test]
+    fn should_error_if_not_given_a_single_argument() {
+        let args = Vec::new();
+        let result = execute(args);
+        assert!(result.is_err());
+    }
+}
