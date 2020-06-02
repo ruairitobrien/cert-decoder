60a3fd41c754f7b09569cc20d0c1ed59ea330736
Ruairi O Brien <ruairitobrien@gmail.com>
2020-06-02 22:11:01 +0100

    Added path exists check

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+
+fn main() -> Result<(), String> {
+    let args = std::env::args().skip(1).collect();
+    execute(args)
 }
 
 fn execute(args: Vec<String>) -> Result<(), String> {
@@ -11,6 +14,13 @@ fn execute(args: Vec<String>) -> Result<(), String> {
         );
         return Err(error);
     }
+    let path = Path::new(&args[0]);
+    if !path.exists() {
+        return Err(
+            "Error: path given as argument does not exist, it must be a path to a certificate!"
+                .to_owned(),
+        );
+    }
     Ok(())
 }
 
@@ -20,22 +30,38 @@ mod test {
     use crate::execute;
 
     #[test]
-fn should_error_if_not_given_a_single_argument() {
-    // arrange
-    let args = Vec::new();
+    fn should_error_if_not_given_a_single_argument() {
+        // arrange
+        let args = Vec::new();
 
-    // act
-    let result = execute(args);
+        // act
+        let result = execute(args);
 
-    // assert
-    assert!(result.is_err());
-    assert_eq!(
-        result.err().unwrap(),
-        format!(
-            "{}{}",
-            "Error: did not receive a single argument, ",
-            "please invoke cert-decoder as follows: ./cert-decoder /path/to/cert."
-        )
-    );
-}
+        // assert
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            format!(
+                "{}{}",
+                "Error: did not receive a single argument, ",
+                "please invoke cert-decoder as follows: ./cert-decoder /path/to/cert."
+            )
+        );
+    }
+
+    #[test]
+    fn should_error_if_argument_is_not_a_path_which_exists() {
+        // arrange
+        let args = vec!["does-not-exist".to_owned()];
+
+        // act
+        let result = execute(args);
+
+        // assert
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            "Error: path given as argument does not exist, it must be a path to a certificate!"
+        );
+    }
 }
