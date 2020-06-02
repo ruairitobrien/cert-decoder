c44df4c194ca410b94e6f2cb189bc24290cd5ef9
Ruairi O Brien <ruairitobrien@gmail.com>
2020-06-02 22:03:05 +0100

    Added useful message to bad arg flow

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn execute(args: Vec<String>) -> Result<(), ()> {
+fn execute(args: Vec<String>) -> Result<(), String> {
     if args.len() != 1 {
-        return Err(());
+        let error = format!(
+            "{}{}",
+            "Error: did not receive a single argument, ",
+            "please invoke cert-decoder as follows: ./cert-decoder /path/to/cert."
+        );
+        return Err(error);
     }
     Ok(())
 }
@@ -15,9 +20,22 @@ mod test {
     use crate::execute;
 
     #[test]
-    fn should_error_if_not_given_a_single_argument() {
-        let args = Vec::new();
-        let result = execute(args);
-        assert!(result.is_err());
-    }
+fn should_error_if_not_given_a_single_argument() {
+    // arrange
+    let args = Vec::new();
+
+    // act
+    let result = execute(args);
+
+    // assert
+    assert!(result.is_err());
+    assert_eq!(
+        result.err().unwrap(),
+        format!(
+            "{}{}",
+            "Error: did not receive a single argument, ",
+            "please invoke cert-decoder as follows: ./cert-decoder /path/to/cert."
+        )
+    );
+}
 }
